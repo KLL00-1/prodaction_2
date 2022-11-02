10be5ffb163e872319f477d0dcf885e3baecf64d
Lev Kochetkov <108297418+KLL00-1@users.noreply.github.com>
2022-11-02 23:13:12 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const btnMenu = document.getElementById("btn_menu");
 const menu = document.querySelector(".menu_btn");
 const navMini = document.querySelector(".nav_mini");
-let number = -50;
+let number = -100;
 
 btnMenu.addEventListener("click", () => {
   menu.classList.toggle("menu-open");
@@ -11,18 +11,18 @@ btnMenu.addEventListener("click", () => {
 function menuOpen() {
   setInterval(() => {
     if (menu.classList != "menu_btn" && number < 0) {
-      number += 1;
-      navMini.style.left = number + "%";
+      number += 2;
+      navMini.style.right = number + "%";
     }
   }, 1);
   setInterval(() => {
     if (
       menu.classList == "menu_btn" &&
       menu.classLists != "menu_open" &&
-      number > -50
+      number > -100
     ) {
-      number -= 1;
-      navMini.style.left = number + "%";
+      number -= 2;
+      navMini.style.right = number + "%";
     }
   }, 1);
 }
