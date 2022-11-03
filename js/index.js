d6749f4d3675ef74400c069275b365aa27889242
Lev Kochetkov <108297418+KLL00-1@users.noreply.github.com>
2022-11-03 13:20:14 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,27 +6,27 @@ const anime2 = document.querySelector(".two");
 const anime3 = document.querySelector(".three");
 const anime4 = document.querySelector(".four");
 const anime5 = document.querySelector(".five");
-let number = -100;
+// let number = -100;
 
 btnMenu.addEventListener("click", () => {
   menu.classList.toggle("menu-open");
-  menuOpen();
+  navMini.classList.toggle("transform");
   addAnimeMenu();
 });
 
-function menuOpen() {
-  setInterval(() => {
-    if (menu.classList != "menu_btn" && number < 0) {
-      number += 1;
-      navMini.style.top = number + "%";
-      navMini.style.right = number + "%";
-    } else if (menu.classList == "menu_btn" && number > -100) {
-      number -= 1;
-      navMini.style.top = number + "%";
-      navMini.style.right = number + "%";
-    }
-  }, 1);
-}
+// function menuOpen() {
+//   setInterval(() => {
+//     if (menu.classList != "menu_btn" && number < 0) {
+//       number += 1;
+//       navMini.style.top = number + "%";
+//       navMini.style.right = number + "%";
+//     } else if (menu.classList == "menu_btn" && number > -100) {
+//       number -= 1;
+//       navMini.style.top = number + "%";
+//       navMini.style.right = number + "%";
+//     }
+//   }, 1);
+// }
 function addAnimeMenu() {
   anime.classList.toggle("anime");
   anime2.classList.toggle("anime");
