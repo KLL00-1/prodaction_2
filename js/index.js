14e7f970eeb4949438e717f307f0590917d6fb4d
Lev Kochetkov <108297418+KLL00-1@users.noreply.github.com>
2022-11-03 04:03:22 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,8 +2,11 @@ const btnMenu = document.getElementById("btn_menu");
 const menu = document.querySelector(".menu_btn");
 const navMini = document.querySelector(".nav_mini");
 const anime = document.querySelector(".one");
-console.log(anime);
-let number = -100;
+const anime2 = document.querySelector(".two");
+const anime3 = document.querySelector(".three");
+const anime4 = document.querySelector(".four");
+const anime5 = document.querySelector(".five");
+let number = 0;
 
 btnMenu.addEventListener("click", () => {
   menu.classList.toggle("menu-open");
@@ -13,17 +16,31 @@ btnMenu.addEventListener("click", () => {
 
 function menuOpen() {
   setInterval(() => {
-    if (menu.classList != "menu_btn" && number < 0) {
+    if (menu.classList != "menu_btn" && number < 100) {
       number += 2;
-      navMini.style.right = number + "%";
-      navMini.style.top = number + "%";
-    } else if (menu.classList == "menu_btn" && number > -100) {
+      navMini.style.height = number + "%";
+      navMini.style.width = number + "%";
+      anime.style.opacity = number + "%";
+      anime2.style.opacity = number + "%";
+      anime3.style.opacity = number + "%";
+      anime4.style.opacity = number + "%";
+      anime5.style.opacity = number + "%";
+    } else if (menu.classList == "menu_btn" && number > 0) {
       number -= 2;
-      navMini.style.right = number + "%";
-      navMini.style.top = number + "%";
+      navMini.style.width = number + "%";
+      navMini.style.height = number + "%";
+      anime.style.opacity = number + "%";
+      anime2.style.opacity = number + "%";
+      anime3.style.opacity = number + "%";
+      anime4.style.opacity = number + "%";
+      anime5.style.opacity = number + "%";
     }
   }, 1);
 }
 function addAnimeMenu() {
   anime.classList.toggle("anime");
+  anime2.classList.toggle("anime");
+  anime3.classList.toggle("anime");
+  anime4.classList.toggle("anime");
+  anime5.classList.toggle("anime");
 }
